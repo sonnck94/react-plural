5786a7c5ef3f545f1cc85177db2b135c8d4a9af3
sondn <sondn@mog.net>
2018-12-05 18:21:20 +0700

    ahihi

--- src/AuthorQuiz.js
+++ src/AuthorQuiz.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import './App.css';
 
-function AuthorQuiz({turnData}){
+function AuthorQuiz({turnData, highlight, onAnswerSelected}){
   return (
     <div className="container-fluid">
         <Hero />
-        <Turn {...turnData}/>
+        <Turn {...turnData} highlight={highlight} onAnswerSelected={onAnswerSelected}/>
         <Continue />
         <Footer />
       </div>
@@ -15,32 +16,51 @@ function AuthorQuiz({turnData}){
 function Hero(){
   return (
     <div className="row">
-      <div class="jumbotron col-10 offset-1">
-      <h1 class="display-4">Author Quiz</h1>
-      <p class="lead">Select book written by the author shown</p>
+      <div className="jumbotron col-10 offset-1">
+      <h1 className="display-4">Author Quiz</h1>
+      <p className="lead">Select book written by the author shown</p>
     </div>
     </div>
   );
 }
-function Book({title}){
+function Book({title, onClick}){
   return(
-    <div className="answer">
+    <div className="answer" onClick={() => onClick(title)}>
        <h4>{title}</h4> 
     </div>
   ); 
 }
 
-function Turn({author, books}){
+function Turn({author, books, highlight, onAnswerSelected}){
+  function highlightBgColor(highlight){
+    const mapping = {
+      'none': '',
+      'correct': 'green',
+      'wrong': 'red'
+    }
+    return mapping[highlight];
+  }
   return (
-  <div className="row turn" style={{backgroundColor: "white"}}>
+  <div className="row turn" style={{backgroundColor: highlightBgColor(highlight)}}>
     <div className="col-4 offset-1">
       <img src={author.imageUrl} className="authorimage" alt="William"></img>
     </div>
     <div className="col-6">
-      {books.map((title) => <Book title={title} key={title} />) }
+      {books.map((title) => <Book title={title} key={title} onClick={onAnswerSelected} />) }
     </div>
   </div>);
 }
+Turn.propTypes = {
+  author: PropTypes.shape({
+     name: PropTypes.string.isRequired,
+     imageUrl: PropTypes.string.isRequired,
+     imageSource: PropTypes.string.isRequired,
+     books: PropTypes.arrayOf(PropTypes.string).isRequired
+  }),
+  books: PropTypes.arrayOf(PropTypes.string).isRequired,
+  onAnswerSelected: PropTypes.func.isRequired,
+  highlight: PropTypes.string.isRequired
+};
 
 function Continue(){
   return (<div> </div>);
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import './bootstrap.min.css';
 import AuthorQuiz from './AuthorQuiz';
 import * as serviceWorker from './serviceWorker';
+import {shuffle, sample} from 'underscore';
 
 const authors = [
     {
@@ -11,15 +12,68 @@ const authors = [
         imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a2/Shakespeare.jpg/220px-Shakespeare.jpg',
         imageSource: 'Wikipedia',
         books: ['The Adventures of Huckleberry Finn', 'Hamless', 'Romeo and Juliet', 'King Lear']
+    },
+    {
+        name: 'Scott Fitzgerald',
+        imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/2/24/F._Scott_Fitzgerald%2C_1921.png',
+        imageSource: 'Wikipedia',
+        books: ['This Side of Paradise', 'The Great Gatsby ', 'The Beautiful and Damned', 'Tender Is the Night']
+    },
+    {
+        name: 'Leo Tolstoy',
+        imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/c/c6/L.N.Tolstoy_Prokudin-Gorsky.jpg',
+        imageSource: 'Wikipedia',
+        books: ['War and Peace', 'The Kingdom of God Is Within You', 'The Death of Ivan Ilyich', 'Resurrection']
+    },
+    {
+        name: 'Ernest Hemingway',
+        imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/28/ErnestHemingway.jpg/220px-ErnestHemingway.jpg',
+        imageSource: 'Wikipedia',
+        books: ['The Sun Also Rises', 'A Farewell to Arms', 'The Old Man and the Sea', 'To Have and Have Not']
+    },
+    {
+        name: 'James Joyce',
+        imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/1e/Revolutionary_Joyce_Better_Contrast.jpg/447px-Revolutionary_Joyce_Better_Contrast.jpg',
+        imageSource: 'Wikipedia',
+        books: ['Ulysses', 'The Dead', 'Everline', 'The sisters']
+    },
+    {
+        name: 'J. K. Rowling',
+        imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/1c/J.K._Rowling_2010-10-24.jpg/421px-J.K._Rowling_2010-10-24.jpg',
+        imageSource: 'Wikipedia',
+        books: ['Harry Potter and the Philosopher\'s Stone', 'Harry Potter and the Chamber of Secrets', 'Harry Potter and the Prisoner of Azkaban', 'Harry Potter and the Goblet of Fire ']
     }
+    
 ]
-const state = {
-    turnData: { 
-        author: authors[0],
-        books: authors[0].books
+function getTurnData(authors){
+    const allBooks = authors.reduce(function(p, c, i){
+        return p.concat(c.books);
+    }, [])
+    const fourRandomBooks = shuffle(allBooks).slice(0, 4);
+    const answer = sample(fourRandomBooks);
+    return {
+        books: fourRandomBooks,
+        author: authors.find((author) => 
+                    author.books.some((title) => 
+                        title === answer))
     }
 }
-ReactDOM.render(<AuthorQuiz {...state}/>, document.getElementById('root'));
+
+const state = {
+    turnData: getTurnData(authors),
+    highlight: ''
+}
+function onAnswerSelected(answer){
+    const isCorrect = state.turnData.author.books.some((book) => book === answer);
+    state.highlight = isCorrect ? 'correct' : 'wrong';
+    render();
+
+}
+function render(){
+    ReactDOM.render(<AuthorQuiz {...state} onAnswerSelected={onAnswerSelected}/>, document.getElementById('root'));
+}
+
+render();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
